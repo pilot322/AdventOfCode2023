9c9a219303813ef1417985bf7164b83e939a1c99
Michalis <michalis.lappas@outlook.com>
2024-11-03 17:39:07 +0200

    first day p1 done

--- Day1/solution.cpp
+++ Day1/solution.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
+int first_digit(string &buf){
+    for(int i = 0; i < buf.length(); i++){
+        if(buf[i] >= '0' && buf[i] <= '9'){
+            return buf[i] - '0';
+        }
+    }
+    return 0;
+}
+
+int last_digit(string &buf){
+    for(int i = buf.length()-1; i >= 0; i--){
+        if(buf[i] >= '0' && buf[i] <= '9'){
+            return buf[i] - '0';
+        }
+    } 
+        
+    return 0;
+}
+
 int main(){
-   cout << "yooo" << endl; 
+    ifstream input("input.txt");
+    string buf;
+    
+    int sum = 0;
+        
+    while(getline(input, buf)){
+        // find first digit
+        //
+        int n = first_digit(buf) * 10 + last_digit(buf);
+        cout << n << endl;
+        sum += n;
+    }
+    
+    input.close();
+
+    cout << sum << endl;
 }
